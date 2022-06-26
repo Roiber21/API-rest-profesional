2dca72a4302cbd70739e874e9303f3d58c6bfa33
roiber21 <roiberale@gmail.com>
2022-06-26 00:34:10 -0500

    agregamos películas en tendencia

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
-async function getTrendingMoviesPreview{
-    const res= await fetch()
-}
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+  
+    const movies = data.results;
+ 
+   movies.forEach(movie => {
+       const trendingPreviewMoviesContainer= document.querySelector('#trendingPreview .tendencias_movies')
+      const movieContainer= document.createElement('div')
+       const movieImg= document.createElement('img')
+       movieImg.classList.add('img_container')
+       movieImg.classList.add('movie-img')
+       movieImg.setAttribute('alt', movie.title)
+       movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path)
+       
+       movieContainer.appendChild(movieImg)
+       trendingPreviewMoviesContainer.appendChild(movieContainer)
+
+   });
+
+  }
+  
+  getTrendingMoviesPreview();
